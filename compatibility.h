cea1f465bb926b60a23a80dd2e124235c208c80f
Raphael Cohn <raphael.cohn@stormmq.com>
2016-05-31 13:26:30 +0100

    Added OS definitions for Linux and Windows

--- compatibility.h
+++ compatibility.h
@@ -15,6 +15,16 @@
 	#if defined(__APPLE__) && defined(__MACH__)
 		#define IsMacOsX 1
 	#endif
+		
+		
+	#if defined(__linux__) || defined(linux) || defined(__linux)
+		#define IsLinuxOrAndroid 1
+	#endif
+		
+		
+	#if defined(_WIN16) || defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+		#define IsWindows 1
+	#endif
 
 
 	#if defined(__GNUC__) && (__GNUC__ >= 4)
